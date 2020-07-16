7c7e531c773dcf90087a130332ae8abcfc2a366c
Sergey P <pavlovdog.moscow@gmail.com>
2020-07-16 12:09:56 +0300

    Update get-contract-details
    - Refactored log according to the contract updates

--- examples/get-contract-details.js
+++ examples/get-contract-details.js
@@ -19,16 +19,20 @@ checkContractAddress(config.contractAddress);
     config.keys,
   );
 
+  // Get distributed status
+  const { value0: distributed_status } = await contract.get_distributed_statusLocal();
+  console.log(`Distributed status: ${distributed_status}`);
+  
   // Contract balance
   const { value0: contract_balance } = await contract.get_current_balanceLocal();
   console.log(`Contract balance: ${parseInt(contract_balance, 16)}`);
   
   
-  // Total amount
-  const { value0: total_amount } = await contract.get_total_amountLocal();
-  console.log(`Total amount: ${parseInt(total_amount, 16)}`);
+  // Required amount
+  const { value0: required_amount } = await contract.get_required_amountLocal();
+  console.log(`Required amount for distribution: ${parseInt(required_amount, 16)}`);
   
-  const balance_status_string = parseInt(contract_balance, 16) > parseInt(total_amount, 16) ? 'Sufficient' : 'Unsufficient';
+  const balance_status_string = parseInt(contract_balance, 16) > parseInt(required_amount, 16) ? 'Sufficient' : 'Unsufficient';
   
   console.log(`\n${balance_status_string} balance\n`);
   
@@ -44,20 +48,11 @@ checkContractAddress(config.contractAddress);
   // Get list of receivers, corresponded amounts and distribution status
   const { value0: addresses } = await contract.get_addressesLocal();
   const { value0: amounts } = await contract.get_amountsLocal();
-
-  const distributedStatus = await Promise
-    .all([...Array(addresses.length).keys()]
-    .map(async (address_index) => {
-      const {
-        value0: distributed_status
-      } = await contract.get_distributed_statusLocal({ i: address_index });
   
-      return distributed_status;
-    }));
   
-  console.log('List of receivers and amounts with distribution status:');
+  console.log('List of receivers and amounts:');
   addresses.map((address, address_index) => {
-    console.log(address_index, distributedStatus[address_index], address, parseInt(amounts[address_index], 16));
+    console.log(address_index, address, parseInt(amounts[address_index], 16));
   });
   
   process.exit(0);
